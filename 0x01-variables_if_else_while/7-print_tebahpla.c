bc6960fc3c0f2cf4095b51378949da2529def2a3
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-09-11 23:32:16 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
  *  main - print if the number is zero, positive or negative
- *
  *  Description - using the main function
- *  This program prints program is positive, zero, or negative
  *  Return: 0.
  */
 int main(void)
 {
 char c;
-for (c= 'z'; c <= 'a'; c--)
+for (c = 'z'; c <= 'a'; c--)
 {
 putchar(c);
 }
