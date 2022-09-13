549b7d8c18e40e4c518032083bb545c7718c6a34
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-09-13 06:55:27 -0700

    Task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * Print_alphabets - a function that prints the alphabet  in lowercase
+ * follow by a new line
  * Return: always 0 (success)
  */
 void print_alphabet(void)
