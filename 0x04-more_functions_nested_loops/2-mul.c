ee46feda7e179018f4d3e79b815250f0234fe851
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-09-15 04:48:52 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -  checks for checks for a digit (0 through 9).
+ * @a: a -  Variable
+ * @b: b - variable
+ */
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
