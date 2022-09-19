7468136eaa836915184fd15ea50b8b0668d918b9
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-09-19 04:09:01 -0700

    Task 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; i < (n - 1); i++)
+printf("%d, ", a[i]);
+if (i == (n - 1))
+printf("%d", a[n - 1]);
+printf("\n");
+}
+
+
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
-A that prints n elements of an array of integers
+a function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.
+
