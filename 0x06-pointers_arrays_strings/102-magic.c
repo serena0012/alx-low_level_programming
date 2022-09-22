88ae8e74bde5e280988b55ed957bf9501fceb8bf
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-09-22 08:59:54 -0700

    Task 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-
+/**
+ *  main - program that print a[2] = 98
+ *
+ *  return: 0
+ */
 int main(void)
 {
 int n;
-int a[5];
+int a[5]; 
 int *p;
 a[2] = 1024;
 p = &n;
- *(p + 5) = 98;
+*(p + 5) = 98;
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
