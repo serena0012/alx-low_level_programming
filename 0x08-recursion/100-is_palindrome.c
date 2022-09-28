a53888f62363c1af1fd4942a35df8ae003ebeedc
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-09-28 01:50:39 -0700

    Task 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * palind2 - obtains length of a
+ * @a: string
+ * @l: integer to count length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int palind2(char *a, int l)
+{
+if (*a == 0)
+return (l - 1);
+return (palind2(a + 1, l + 1));
+}
+/**
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int is_palindrome(char *s)
+{
+int l;
+l = palind2(s, 0);
+return (palind3(s, l));
+}
+
