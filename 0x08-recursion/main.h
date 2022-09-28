7101bf1b8ca6bbf95241afb5a33035f767f3897a
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-09-28 02:01:48 -0700

    Task 100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,8 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int palind3(char *a, int l)
+int palind2(char *a, int l)
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
