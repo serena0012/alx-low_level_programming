9d653acc812e996d26ddc57e78f0aba492a20538
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-02 07:51:00 -0700

    Task 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: Gbenga Elegbede
+ */
+
+#include "main.h"
+/**
+ * _isalpha - this program checks wether a character
+ * is from the English alphabet.
+ * Return: 1 for English Character
+ * 0 if the character is not English
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
