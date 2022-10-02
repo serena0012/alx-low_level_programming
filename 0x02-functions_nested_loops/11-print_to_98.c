c680e6f1044fcd65b71c9a5e9c5053ba363ccd6c
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-02 09:18:01 -0700

    Task 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: Gbenga Elegbede
+ */
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98.
+ * @n: parameter
+ * Return: void.
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+}
