1a2d3a84616fa9f95ca0997b7b33adb36e396420
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-02 08:28:12 -0700

    Task 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Gbenga Elegbede
+ */
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a numer.
+ * @n: parameter.
+ * Return:last digit.
+ */
+int print_last_digit(int n)
+{
+int x;
+x = n % 10;
+if (x < 0)
+x = x * (-1);
+_putchar(x + '0');
+return (x);
+}
