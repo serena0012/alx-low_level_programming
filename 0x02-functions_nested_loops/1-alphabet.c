e984d26096a9659de5bfde7095ad2cf22aee659d
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-02 01:56:19 -0700

    Task 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Gbenga Elegbede
+ */
+#include "main.h"
+/**
+ * print_alphabet -This program prints
+ * the english alphabet from a-z.
+ * Return Void.
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
