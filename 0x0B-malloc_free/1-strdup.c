e4122dffa68a836dfdb08dbed5f5864b403e0755
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-05 06:11:22 -0700

    Task 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,37 @@
-#include "main."
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * _strdup - Entry point
- * @str: string we need to duplicate
- * Return: a pointer to the duplicated string or NULL
+ * _strdup - write a function that returns a pointer to a newly allocated space
+ * in memeory which contains a copy of the string given as a parameter
+ * Prototype: char *_strdup(char *str);
+ * The _strdup() function returns a pointer to a new string which is a
+ * duplicate of the string str. Memory for the new string is
+ * obtained with malloc, and can be freed with free.
+ * Returns NULL if str = NULL
+ * On success, the _strdup function returns a pointer to the duplicated string.
+ * It returns NULL if insufficient memory was available
+ *
+ * @str: string.
+ *
+ * Return: pointer of an array of chars
  */
+
 char *_strdup(char *str)
 {
-char *strnew = NULL;
-unsigned int i;
-int n;
+char *strout;
+
+unsigned int i, j;
 
 if (str == NULL)
 return (NULL);
-for (n = 0; str[n] != '\0'; n++)
-strnew = (char *)malloc(n + 1 * sizeof(char));
-if (strnew != NULL)
-{
 for (i = 0; str[i] != '\0'; i++)
-strnew[i] = str[i];
-}
-else
-{
+;
+strout = (char *)malloc(sizeof(char) * (i + 1));
+if (strout == NULL)
 return (NULL);
-}
-strnew[i] = '\0';
-return (strnew);
+for (j = 0; j <= i; j++)
+strout[j] = str[j];
+return (strout);
 }
 
