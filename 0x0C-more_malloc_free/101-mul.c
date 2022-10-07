c85667120a10be33b29401b70841155b0d141bb5
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-07 05:10:49 -0700

    Fixed

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
 int find_len(char *str);
 char *create_xarray(int size);
 char *iterate_zeroes(char *str);
@@ -14,10 +12,7 @@ void add_nums(char *final_prod, char *next_prod, int next_len);
  * find_len - Finds the length of a string.
  * @str: The string to be measured.
  *
- *   
- *
  * Return: The length of the string.
- *
  */
 
 int find_len(char *str)
@@ -31,24 +26,25 @@ return (len);
 /**
  * create_xarray - Creates an array of chars and initializes it with
  * the character 'x'. Adds a terminating null byte.
- *
  * @size: The size of the array to be initialized.
  *
  * Description - if is there is insufficient space, the
- *
  * function exits with a status of 98.
- *
  * Return: A pointer to the array.
  */
 char *create_xarray(int size)
 {
 char *array;
 int index;
+
 array = malloc(sizeof(char) * size);
+
 if (!array)
 exit(98);
+
 for (index = 0; index < (size - 1); index++)
 array[index] = 'x';
+
 array[index] = '\0';
 return (array);
 }
@@ -58,7 +54,6 @@ return (array);
  * leading zeroes until it hits a non-zero number.
  *
  * @str: The string of numbers to be iterate through.
- *
  * Return: A pointer to the next non-zero element.
  */
 
@@ -70,15 +65,12 @@ return (str);
 }
 
 /**
- *
  * get_digit - Converts a digit character to a corresponding int.
  * @c: The character to be converted.
  *
  * Description: If c is a non-digit, the function
  * exits with a status of 98.
- *
  * Return: The converted int.
- *
  */
 
 int get_digit(char c)
