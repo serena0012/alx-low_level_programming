d549d8e92a0a49c129f051029f6ad44f1eb72b3c
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-07 02:50:02 -0700

    Task1 fixedd

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,32 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * array_range - get len, malloc * len
- * loop size of len to insert min++ into new arr
- * @min: min input
- * @max: max input
- * Return: pointer to new arr of ints
+ *  string_nconcat - Concatenates two strings using at
+ *  most an inputted number of bytes.
+ *  @s1: The first string.
+ *  @s2: The second string.
+ *  @n: The maximum number of bytes of s2 to concatenate to s1.
+ *  Return: If the function fails - NULL.
+ *  Otherwise - a pointer to the concatenated space in memory.
+ *
  */
-int *array_range(int min, int max)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int *arr;
-int i, len;
+char *concat;
+unsigned int len = n, i;
 
-if (min > max)
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+for (i = 0; s1[i]; i++)
+len++;
+
+concat = malloc(sizeof(char) * (len + 1));
+
+if (!concat)
 return (NULL);
 
-for (len = 0; len <= (max - min); len++)
-{
-}
-arr = malloc(sizeof(int *) * len);
-if (arr == NULL)
-return (NULL);
+len = 0;
 
-for (i = 0; i <= len; i++)
-{
-arr[i] = min++;
+for (i = 0; s1[i]; i++)
+concat[len++] = s1[i];
+
+for (i = 0; s2[i] && i < n; i++)
+concat[len++] = s2[i];
+
+concat[len] = '\0';
+
+return (concat);
 }
-return (arr);
-}
+
