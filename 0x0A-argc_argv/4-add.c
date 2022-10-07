5ac93706fee80f3d0bff5fe284b6ee1bf519d5ff
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-07 14:01:24 -0700

    Fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,97 @@
 #include <stdio.h>
+
 #include <stdlib.h>
-#include <string.h>
+
 #include <ctype.h>
-#include "main.h"
+
+
 
 /**
- * main - Program that takes in all integer arguments and returns the sum
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+
+ * main - Write a program that adds positive numbers
+
+ * Print the result, followed by a new line
+
+ * If no number is passed to the program, print 0, followed by a new line
+
+ * If one of the number contains symbols that are not digits,
+
+ * print Error, followed by a new line, and return 1
+
+ * You can assume that numbers and the addition of all
+
+ * the numbers can be stored in an int
+
+ *
+
+ * @argc: Argument count
+
+ * @argv: Array of argument strings
+
+ *
+
+ * Return: 0 for successful exit
+
  */
+
 int main(int argc, char *argv[])
-{
-int i, j, length, sum;
-char *ptr;
-if (argc < 2)
-printf("0\n");
-else
-{
-sum = 0;
-for (i = 1; i < argc; i++)
-{
-ptr = argv[i];
-length = strlen(ptr);
-for (j = 0; j < length; j++)
+
 {
 
+
+
+	int i = 0;
+
+	int sum = 0;
+
+
+
+	if (argc > 1)
+
+	{
+
+		for (i = 1; i < argc; i++)
+
+		{
+
+			int b;
+
+			char *str;
+
+
+
+			str = argv[i];
+
+			for (b = 0; str[b] != '\0'; b++)
+
+			{
+
+				if (str[b] < 48 || str[b] > 57)
+
+				{
+
+					printf("Error\n");
+
+					return (1);
+
+				}
+
+			}
+
+		}
+
+	}
+
+	for (i = 1; i < argc; i++)
+
+	{
+
+		sum += atoi(argv[i]);
+
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+
+}
