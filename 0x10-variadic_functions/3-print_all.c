fe5d461aeca555d899735f3b21e3ab72414071b0
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-14 16:28:18 -0700

    3-print_all.c .Fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,45 +11,37 @@
 void print_all(const char * const format, ...)
 {
 va_list valist;
-unsigned int i = 0, j, c = 0;
-char *str;
-const char t_arg[] = "cifs";
+int i = 0;
+char *s;
+
+
 va_start(valist, format);
+
 while (format && format[i])
 {
-j = 0;
-while (t_arg[j])
-{
-if (format[i] == t_arg[j] && c)
-{
-printf(", ");
-break;
-}
-j++;
-}
-switch (format[j])
+switch (format[i++])
 {
 case 'c':
-printf("%c", va_arg(valist, int)), c = 1;
+printf("%c", va_arg(valist, int));
 break;
 case 'i':
-printf("%d", va_arg(valist, int)), c = 1;
+printf("%d", va_arg(valist, int));
 break;
 case 'f':
-printf("%f", va_arg(valist, double)), c = 1;
+printf("%f", (float)va_arg(valist, double));
 break;
 case 's':
-str = va_arg(valist, char *), c = 1;
-if (!str)
-{
-printf("(nil)");
+s = va_arg(valist, char *);
+if (!s)
+s = "(nil)";
+printf("%s", s);
 break;
+default:
+continue;
 }
-{
-printf("%s", str);
-break;
-} i++;
+if (format[i])
+printf(", ");
 }
-printf("\n"), va_end(valist);
-}
+printf("\n");
+va_end(valist);
 }
