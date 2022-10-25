75e21d0a9184e5e261e0fe48ead07311a907df34
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-25 03:02:23 -0700

    Betty fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,13 +5,13 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
-	if (head == NULL)
-		return;
-	while (*head != NULL)
-	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
-	}
+listint_t *temp;
+if (head == NULL)
+return;
+while (*head != NULL)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
 }
