fc9a97968182d15817619494495a66e30ce4230e
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-10-27 17:05:01 +0100

    Task 1-5

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *get_bit - returns the value of a bit at a given index
+ *@n: number to convert
+ *@index: index to search
+ *Return: byte in the index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > ((sizeof(n) * 8) - 1))
+		return (-1);
+	return ((n >> index) & 1);
+}
